f0fa55d745540cd571ef0969969c53a8b3ddd916
Andrii Konovalov <andy.smokk@gmail.com>
2022-07-18 23:02:51 +0200

    fix url post

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
 const axios = require("axios").default;
-const BASE_URL_FETCH = "https://62cd6e7d066bd2b69925bcaf.mockapi.io/api/v1/";
-const BASE_URL_POST =
-  "http://back-delivery-app.herokuapp.com/orders/shopping-cart";
+const BASE_URL_FETCH = "https://62cd6e7d066bd2b69925bcaf.mockapi.io/api/v1";
+const BASE_URL_POST = "https://back-delivery-app.herokuapp.com";
 
 async function fetchShops() {
   try {
@@ -23,7 +22,10 @@ async function fetchProducts() {
 
 async function sendOrder(data) {
   try {
-    const response = await axios.post(BASE_URL_POST, data);
+    const response = await axios.post(
+      `${BASE_URL_POST}/orders/shopping-cart`,
+      data
+    );
     return response;
   } catch (error) {
     console.error(error.message);
